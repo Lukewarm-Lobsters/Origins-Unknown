a0360df6a34b5b52651023ae6a1db4eb2ba15b2f
Joey Remmers <joeyjoeyremmers@gmail.com>
2023-10-11 14:57:20 +0200

    added target resetter button

--- Origins Unknown/Assets/_Project/Scripts/ResetTargets.cs
+++ Origins Unknown/Assets/_Project/Scripts/ResetTargets.cs
@@ -6,16 +6,49 @@ namespace LukewarmLobsters.OriginsUnknown
 {
     public class ResetTargets : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public List<Target> targetList = new List<Target>();
+
+        private void Start()
         {
-        
+
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Target"))
+            {
+                targetList.Add(new Target(obj, obj.transform.position, obj.transform.rotation));
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnCollisionEnter(Collision other)
         {
-        
+
+            if (other.gameObject.tag == "Player")
+            {
+                print("HE'S TOUCHING ME!!!");
+                foreach (Target obj in targetList)
+                {
+                    obj.target.transform.position = obj.position;
+                    obj.target.transform.rotation = obj.rotation;
+                    obj.target.GetComponent<Rigidbody>().velocity = new Vector3();
+                }
+            }
+        }
+    }
+
+    public class Target
+    {
+        public GameObject target;
+        public Vector3 position;
+        public Quaternion rotation;
+
+        public Target(GameObject _target, Vector3 _position, Quaternion _rotation)
+        {
+            target = _target;
+            position = _position;
+            rotation = _rotation;
+        }
+
+        public override string ToString()
+        {
+            return $"name: {target.name}";
         }
     }
 }
