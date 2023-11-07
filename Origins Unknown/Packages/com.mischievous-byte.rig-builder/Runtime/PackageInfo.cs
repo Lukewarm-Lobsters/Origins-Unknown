07c25eda12136a5eb3c2d160370e82a65edd0745
Laurens Kruis <laurens@kruis.name>
2023-11-07 10:00:43 +0100

    Added Rig-Builder

---
+++ Origins Unknown/Packages/com.mischievous-byte.rig-builder/Runtime/PackageInfo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MischievousByte.RigBuilder
+{
+    public static class PackageInfo
+    {
+        public const string DevelopmentName = "Rig Builder";
+
+        public const string PackageName = "com.mischievous-byte.rig-builder";
+        public const string PackagePath = "Packages/" + PackageName + "/";
+
+        public const string ComponentMenuPrefix = DevelopmentName + "/";
+    }
+}
