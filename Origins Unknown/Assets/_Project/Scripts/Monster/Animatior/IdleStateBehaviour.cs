756295de34476d0256ec56322861b39706466ee9
Joey Remmers <joeyjoeyremmers@gmail.com>
2023-11-13 15:31:21 +0100

    added FOV playerdetector
    I also moved some scripts around

---
+++ Origins Unknown/Assets/_Project/Scripts/Monster/Animatior/IdleStateBehaviour.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LukewarmLobsters.OriginsUnknown
+{
+    public class IdleStateBehaviour : StateMachineBehaviour
+    {
+        float timer;
+        Transform playerTransform;
+
+        [SerializeField] private float minIdleTime = 1;
+        [SerializeField] private float maxIdleTime = 4;
+
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+            timer = 0;
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            timer += Time.deltaTime;
+
+            if (timer > Random.Range(minIdleTime, maxIdleTime))
+            {
+                animator.SetBool("isPatrolling", true);
+            }
+
+        }
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            
+        }
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
